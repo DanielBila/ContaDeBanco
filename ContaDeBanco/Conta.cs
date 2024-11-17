73d884b56e11080cd124e9977eeb4efea45482e9
danie <danie@DANIEL>
2024-11-17 10:15:01 -0300

    Adicionar arquivos de projeto.

---
+++ ContaDeBanco/Conta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ContaDeBanco
+{
+    internal class Conta
+    {   
+        private string _titular;
+        private string _numero;
+        private int _cpf;
+        public double Saldo { get; private set; }
+
+        public Conta(string titular,int cpf)
+        {
+            Random random = new Random();
+            _titular = titular ;
+            _cpf = cpf;
+            Saldo = 0.0;
+            //Este metodos fazer que criam 5 numeros aleatorios que serão o numero da conta 
+            _numero = $"{random.Next(1, 9)}{random.Next(1, 9)}{random.Next(1, 9)}{random.Next(1, 9)}{random.Next(1, 9)}";                                                                
+        }
+        public Conta(string titular, int cpf,double saldo) 
+        {
+            Random random = new Random();
+            _titular = titular;
+            _cpf = cpf;
+            Saldo = saldo;
+            //Este metodos fazer que criam 5 numeros aleatorios que serão o numero da conta 
+            _numero = $"{random.Next(1, 9)}{random.Next(1, 9)}{random.Next(1, 9)}{random.Next(1, 9)}{random.Next(1, 9)}";
+        }
+        public void Sacar(double dinhero)
+        {
+            Saldo -= dinhero;
+        }
+        public void Depositar(double dinhero)
+        {
+            Saldo += dinhero;
+        }
+        public override string ToString()
+        {
+            return "Conta: " + _numero + 
+                   "; Titular: " + _titular +
+                   "; Saldo: R$" + Saldo;
+        }
+    }
+}
---
+++ ContaDeBanco/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+
+namespace ContaDeBanco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            //Incerção de Informacoes do Cliente 
+            Console.WriteLine("Digite teu nome Completo:");
+            string titular = Console.ReadLine();          
+            Console.WriteLine($"Perfeito {titular.Split(' ').First()} agora Digite o seu CPF:");
+            int cpf = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Perfeito Sua Conta Criado");
+            string inicial = null;
+            Conta conta = null;
+            //Deposito Inicial
+            do
+            {   
+                Console.Clear();
+                Console.WriteLine("Haverá depósito inicial (s/n)?");
+                inicial = Console.ReadLine();
+                if(inicial != "s" &&  inicial != "S" && inicial != "n" && inicial != "N")
+                {
+                    Console.WriteLine("Celeção Invalida");
+                    Task.Delay(500).Wait();
+                }
+
+
+            } while (inicial != "s" && inicial != "S" && inicial != "n" && inicial != "N");
+            //Aceitou Deposito Inicial
+            if(inicial == "s" || inicial == "S")
+            {
+                Console.Clear();
+                Console.WriteLine("Perfeito Qual Seria o valor do deposito");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                conta = new Conta(titular,cpf,depositoInicial);
+            }
+            else if(inicial == "n" || inicial == "N")
+            {
+                Console.Clear();
+                conta = new Conta(titular,cpf);
+            }
+            Console.WriteLine("Dados da Conta:");
+            Console.WriteLine(conta);
+            Console.ReadLine();
+            bool loop = true;
+            while (loop) 
+            {
+                Console.Clear();
+                Console.WriteLine("1-Dados da Conta");
+                Console.WriteLine("2-Depositar");
+                Console.WriteLine("3-Sacar");
+                Console.WriteLine("4-Sair");
+                int escolha = int.Parse(Console.ReadLine());
+                //Dados da Conta 
+                if (escolha == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Dados da Conta:");
+                    Console.WriteLine(conta);
+                    Console.ReadLine();
+                }
+                //Depositar
+                else if (escolha == 2) 
+                {
+                    Console.Clear();
+                    Console.WriteLine("Qual Seria o valor do deposito");
+                    double deposito = double.Parse(Console.ReadLine());
+                    conta.Depositar(deposito);
+                }
+                //Sacar
+                else if(escolha == 3)
+                {
+                    double sacar;
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"Saldo Atual R${conta.Saldo}");
+                        Console.WriteLine("Qual Seria o valor do Sacado");
+                        sacar = double.Parse(Console.ReadLine());
+                        if(conta.Saldo < sacar)
+                        {
+                            Console.WriteLine("Valor invalido Saldo Indisponivel");
+                            Task.Delay(1000).Wait();
+                        }
+                    } while (conta.Saldo < sacar);                                
+                    conta.Sacar(sacar);
+                                        
+                }
+                //sair
+                else if (escolha == 4)
+                {
+                    Console.Clear();
+                    loop = false;
+                }
+            }        
+        }
+    }
+}
